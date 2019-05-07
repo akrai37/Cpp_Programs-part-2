219bb6c9522718f9db853a522cc1f52426d89fd5
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-07 21:59:02 +0530

    freeing_the_pointer
    Dangling pointer is the pointer whose value has been deleted at the location but still points to the same memory location. by using delete keyword you can just delete the value, the pointer points to in this case is deleting the value 10(any value) but it's still pointing to same location which is called dangling pointer , in order to remove this pointing, we use null pointer or assign null value to the pointer so it removes the pointing to that location in memory.

---
+++ DynamicAllocateArrrays.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+//best topic till now
+int main()
+{
+    int *pointer;
+    int input;
+
+    cout<<"enter the no. of inputs : "<<endl;
+    cin>>input;
+
+    pointer=new int[input];
+
+    int temp;
+    int i;
+
+    for(i=0; i<input; i++){
+
+        cout<<"enter the item:"<<i+1<<endl;
+        cin>>temp;
+        *(pointer+i)=temp;
+    };
+
+    for(i=0; i<input; i++){
+
+      cout<<"item - "<<*(pointer+i)<<endl;
+    }
+
+    delete pointer;
+    /*in this program there was no need to dynamically allocate array but just for illustration purpose we did that.*/
+
+
+ return 0;
+}
+
