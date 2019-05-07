1a52e7de4802cce2b17ea563e9b65b6b12236715
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-07 22:06:16 +0530

    Common_Datatype
    -"auto" has the ability to take any datatype value without declaring the datatype. it will automatically find the datatype but you will have to INITIALIZE the variable where it is declared , otherwise it will show the error . syntax- auto age=10;

---
+++ AutoDatatype.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int sum(){
+
+int a=45;
+int b=34;
+return a+b;
+
+}
+int main(){
+
+auto age=10;
+cout<<age<<endl;
+
+auto summation=sum();
+cout<<summation<<endl;
+
+return 0;
+}
