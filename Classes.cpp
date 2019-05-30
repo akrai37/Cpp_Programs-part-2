b0bc703d898865c9f6b1d3b9dc5fe1750346f8be
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-30 21:45:32 +0530

    Introduction_to_classes
    Classes are similar to the structures. They are user difined datatype containing variables , methods and is called in the main fn using the variable of the class. But there are some differences. It can have children classes that can acquire the properties of parent classes . A class has all members private by default. A struct is a class where members are public by default. ex-class student{ };  int main( ){ student anil; }

---
+++ Classes.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+
+using namespace std;
+
+class Human{
+
+public:
+
+    void display(){
+
+        cout<<"have a good day"<<endl;
+
+    }
+};
+
+int main(){
+
+    Human ronit;
+
+    ronit.display();
+
+    return 0;
+
+}
